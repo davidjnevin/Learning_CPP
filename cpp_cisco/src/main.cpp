5f53faa01fca986c875cba26a4cd3c2786748e86
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-15 18:59:40 +0200

    fibonacci

--- cpp_cisco/src/main.cpp
+++ cpp_cisco/src/main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-  int x = 7;
-  int y = 5;
-  int a =
-      (x > y) ? x : y; // here we are using conditional expression to evaluate
-  cout << "value of a using conditional expression is: " << a << endl;
-  // this is equivalent to:
+void test(int range)
+{
+    int first = 0, second = 1, fibonacci = 0;
+    for ( int c = 0 ; c < range ; c++ )
+    {
+        if ( c <= 1 ){
+            fibonacci = c;
+        }else
+        {
+            fibonacci = first + second;
+            first = second;
+            second = fibonacci;
+        }
+        cout << fibonacci <<" ";
+    }
+}
 
-  if (x > y) { // here we are usig if-else which will gave same output
-    a = x;
-    cout << "value of a using if-else is: " << a << endl;
-  } else {
-    a = y;
-    cout << "value of a using if-else is: " << a << endl;
-  }
-  return 0;
+int main(){
+	test(10);
+	return 0;
 }
