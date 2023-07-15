e90cccc01b67e8c2abd0acbd18c0f482419430be
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-15 15:19:24 +0200

    Do while find largest number

--- cpp_cisco/src/main.cpp
+++ cpp_cisco/src/main.cpp
@@ -4,28 +4,18 @@ using namespace std;
 
 int main()
 {
-	/* we will count the numbers here */
-	int evens = 0, odds = 0;
-
-	/* we will store the incoming numbers here */
-	int number;
-
-	/* read first number */
-	cin >> number;
-
-	/* 0 terminates execution */
-	while (number != 0) {
-		/* check if the number is odd */
-		if (number % 2 == 1)
-			/* increase „odd” counter */
-			odds++;
-		else
-			/* increase „even” counter */
-			evens++;
-		/* read next number */
-		cin >> number;
-	}
-	/* print results */
-	cout << "Even numbers: " << evens << endl;
-	cout << "Odd numbers: " << odds << endl;
+    int number;
+    int max = -100000;
+    int counter = 0;
+    do {
+        cin >> number;
+        if (number != -1)
+            counter++;
+        if (number > max)
+            max = number;
+    } while (number != -1);
+    if (counter)
+        cout << "The largest number is " << max << endl;
+    else
+        cout << "Are you kidding? You haven't entered any number!" << endl;
 }
