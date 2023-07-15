3ae5916541efd2fa5a34e0d8c338370854a8e3a4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-15 15:08:19 +0200

    Max of three ints

--- cpp_cisco/src/main.cpp
+++ cpp_cisco/src/main.cpp
@@ -1,27 +1,31 @@
-/* finding the larger of two numbers */
+/* finding the largest of three numbers */
 
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-  /* the two numbers */
-  int number1, number2;
+  /* the three numbers */
+  int number1, number2, number3;
 
-  /* read two numbers */
+  /* read three numbers */
   cin >> number1;
   cin >> number2;
+  cin >> number3;
 
   /* we will save the larger number here */
   /* we temporarily assume that the former number is the larger one */
   /* we will check it soon */
   int max = number1;
 
-  /* we check if the assumption was false */
+  /* we check if the second value is the largest */
   if (number2 > max)
       max = number2;
 
+  /* we check if the third value is the largest */
+  if (number3 > max)
+      max = number3;
+
   /* we print the result */
-  cout << "The larger number is " << max << endl;
+  cout << "The largest number is " << max << endl;
 }
