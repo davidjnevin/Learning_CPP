a79e83e1eced5b8d0f71f069c95e668673a8d11a
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-15 17:57:11 +0200

    ternary expressions

--- cpp_cisco/src/main.cpp
+++ cpp_cisco/src/main.cpp
@@ -2,21 +2,20 @@
 
 using namespace std;
 
-int main()
-{
-int x = 2;
-  int temp;
-    switch (x) {
-    case 1:
-        temp = 1;
-        break;
-    case 2:
-        temp = 2;
-        break;
-    default:
-        temp = 3;
-        break;
-    }
-  cout << "Value of temp is: "<< temp << endl;
+int main() {
+  int x = 7;
+  int y = 5;
+  int a =
+      (x > y) ? x : y; // here we are using conditional expression to evaluate
+  cout << "value of a using conditional expression is: " << a << endl;
+  // this is equivalent to:
+
+  if (x > y) { // here we are usig if-else which will gave same output
+    a = x;
+    cout << "value of a using if-else is: " << a << endl;
+  } else {
+    a = y;
+    cout << "value of a using if-else is: " << a << endl;
+  }
   return 0;
 }
