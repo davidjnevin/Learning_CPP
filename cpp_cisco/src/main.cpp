156a23630fbd169479ce0bc6bb0ad0954c882943
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-16 12:06:57 +0200

    Add google cpp indentation

--- cpp_cisco/src/main.cpp
+++ cpp_cisco/src/main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void test(int range)
-{
-    int first = 0, second = 1, fibonacci = 0;
-    for ( int c = 0 ; c < range ; c++ )
-    {
-        if ( c <= 1 ){
-            fibonacci = c;
-        }else
-        {
-            fibonacci = first + second;
-            first = second;
-            second = fibonacci;
-        }
-        cout << fibonacci <<" ";
-    }
+void test(int range) {
+	int first = 0, second = 1, fibonacci = 0;
+	for (int c = 0; c < range; c++) {
+		if (c <= 1) {
+			fibonacci = c;
+		} else {
+			fibonacci = first + second;
+			first = second;
+			second = fibonacci;
+		}
+		cout << fibonacci << " ";
+	}
 }
 
-int main(){
+int main() {
 	test(10);
 	return 0;
 }
