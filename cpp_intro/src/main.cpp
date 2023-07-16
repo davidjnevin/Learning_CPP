5347599b0b5f61d6bd1d62fe0b4c16ae9164818a
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-16 21:05:22 +0200

    Rename to intro

---
+++ cpp_intro/src/main.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+int func(int number) {
+
+ if (abs(number)/10 == 0) {
+   return 1;
+ }
+ else {
+   return 1 + func(number / 10);
+ }
+}
+int main() {
+		int number = 86250;
+	std::cout >> func(number);
+}
